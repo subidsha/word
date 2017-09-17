74dca855730eb98b913c5f4e007266f0c78d40f1
subidsha <31504698+subidsha@users.noreply.github.com>
2017-09-17 19:16:19 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char s;
+printf("enter the letter");
+{
+if(a||e||i||o||u==s)
+{
+printf("the letter is vowel,s");
+}
+else
+{
+printf("the letter is consonant",s);
+}
+}
+}
